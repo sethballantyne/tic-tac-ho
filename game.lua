15fdfe6f9cfc82ea25d610e739d1fd59641608eb
Seth Ballantyne <seth.ballantyne@gmail.com>
2021-04-01 04:20:17 +1300

    Update game.lua

--- game.lua
+++ game.lua
@@ -26,6 +26,9 @@ TR_TO_BL_LINE = 			BOARD_TOP_RIGHT | BOARD_MIDDLE_CENTER | BOARD_BOTTOM_LEFT
 TURN_PLAYER = 0
 TURN_AI = 0
 
+PLAYER_AI = 0
+PLAYER_MEAT_BAG = 1
+
 KEY_SPACE = 32
 BUTTON_LEFT = 1
 
@@ -51,6 +54,12 @@ gameState = GAME_STATE_TITLE_SCREEN
 playerBoard = 0;
 aiBoard = 0;
 
+winningCombinations = { TOP_HORIZ_LINE, MIDDLE_HORIZ_LINE, BOTTOM_HORIZ_LINE, LEFT_VERTICAL_LINE,
+						MIDDLE_VERTICAL_LINE, RIGHT_VERTICAL_LINE, TL_TO_BR_LINE, TR_TO_BL_LINE
+					}
+
+round = 0;
+
 function BuildCellCoords(startingRow)
 	for y = 1, 3 do
 		yCellHeightStep = cellHeight * (y - 1);
@@ -70,8 +79,8 @@ function BuildCellCoords(startingRow)
 end
 
 function ChangeState()
- gameState = gameState + 1
- Console_Print(gameState);
+	gameState = gameState + 1
+	Console_Print(gameState);
 end
 
 function DrawTitleScreen()
@@ -85,40 +94,60 @@ function DrawTitleScreen()
 	Sprite_Draw(logoSprite, 0, 420);
 end
 
-function DrawDrawScreen()
-	BmpFont_PrintLine(bmpFont, 20, "IT'S A GAPER!");
-	BmpFont_PrintLine(bmpFont, 443, "PRESS SPACE TO PLAY AGAIN! YAS!");
-end
-
-function DrawGameScreen()
+function DrawBoard()
 	Sprite_Draw(boardSprite, boardCoords[1], boardCoords[2]);
 
 	for i = 1, 9 do
 		local bit = 1 << (i - 1);
 		if playerBoard & bit == bit then
 			DrawQuad(cellCoords[i][1], cellCoords[i][2], cellWidth, cellHeight, 255, 0, 0)
+		elseif aiBoard & bit == bit then
+			DrawQuad(cellCoords[i][1], cellCoords[i][2], cellWidth, cellHeight, 255, 255, 0)
 		end
 	end
 end
 
+function DrawDrawScreen()
+	BmpFont_PrintLine(bmpFont, 20, "IT'S A GAPER!");
+	DrawBoard()
+	BmpFont_PrintLine(bmpFont, 443, "PRESS SPACE TO PLAY AGAIN! YAS!");
+end
+
+function DrawGameScreen()
+	if turn == PLAYER_MEAT_BAG then
+		BmpFont_PrintLine(bmpFont, 20, string.format("ROUND %d", round));
+	end
+
+	DrawBoard()
+end
+
 function DrawLossScreen()
 	BmpFont_PrintLine(bmpFont, 20, "YOU LOST. HA.");
+	DrawBoard()
 	BmpFont_PrintLine(bmpFont, 443, "PRESS SPACE TO PLAY AGAIN! YAS!");
 end
 
 function DrawWinScreen()
 	BmpFont_PrintLine(bmpFont, 20, "YOU WIN, PIMP!");
+	DrawBoard()
 	BmpFont_PrintLine(bmpFont, 443, "PRESS SPACE TO PLAY AGAIN! YAS!");
 end
 
 function SpaceKeyPressed()
-	if gameState == GAME_STATE_TITLE_SCREEN then
-		gameState = GAME_STATE_PLAYING
+	if gameState == GAME_STATE_TITLE_SCREEN or
+	   gameState == GAME_STATE_LOSS or
+	   gameState == GAME_STATE_WIN or
+	   gameState == GAME_STATE_DRAW then
+			aiBoard = 0
+			playerBoard = 0
+			gameState = GAME_STATE_PLAYING
+			turn = math.random(0, 1)
+			round = round + 1
 	end
 end
 
 function MouseButtonClicked()
-	if gameState == GAME_STATE_PLAYING and turn == TURN_PLAYER then
+	if gameState == GAME_STATE_PLAYING and turn == PLAYER_MEAT_BAG then
 		local x, y = Input_GetMouseXY()
 		for i = 1, 9 do
 			local xExpr = (x >= cellCoords[i][1]) and (x <= cellCoords[i][3]);
@@ -126,8 +155,10 @@ function MouseButtonClicked()
 
 			if xExpr and yExpr then
 				local bit = 1 << (i - 1);
-				if playerBoard & bit ~= bit then
+				if playerBoard & bit ~= bit and aiBoard & bit ~= bit then
+					Console_Print("Placing players piece");
 					playerBoard = (playerBoard | bit);
+					turn = PLAYER_AI
 				end
 
 				return
@@ -149,8 +180,109 @@ function Create()
     BuildCellCoords()
 end
 
-function Update()
+function CellIsEmpty(bit)
+	return ((playerBoard | aiBoard) & bit) ~= bit
+end
 
+function CheckForPossibleWin(playerType, bit)
+
+	local tempBoard = 0
+
+	if playerType == PLAYER_AI then
+		tempBoard = aiBoard | bit
+	else
+		tempBoard = playerBoard | bit
+	end
+
+	for i = 1, 8 do
+		if tempBoard & winningCombinations[i] == winningCombinations[i] then
+			Console_Print(string.format("CheckForPossibleWin: Winning combination %d hit. Index: %d", winningCombinations[i], i))
+			return 1
+		end
+	end
+
+	return 0
+end
+
+function PlacePiece(playerType, bit)
+	if playerType == PLAYER_AI then
+		aiBoard = aiBoard | bit;
+	else
+		playerBoard = playerBoard | bit;
+	end
+end
+
+function ProcessAI()
+	Console_Print("AI taking it's turn.");
+	-- check for any possible moves that will result in the AI winning in this round.
+	for i = 1, 9 do
+		local bit = 1 << (i - 1)
+
+		if CellIsEmpty(bit) == true then
+			local result = CheckForPossibleWin(PLAYER_AI, bit)
+			if result == 1 then
+				Console_Print(string.format("AI making a winning move at cell index %d", i));
+				PlacePiece(PLAYER_AI, bit)
+				return 2
+			end
+		end
+	end
+
+	-- no possible wins during this turn, so check to see if there's a possible win
+	-- for the player and block it.
+	for i = 1, 9 do
+		local bit = 1 << (i - 1)
+
+		if CellIsEmpty(bit) == true then
+			local result = CheckForPossibleWin(PLAYER_MEAT_BAG, bit)
+			if result == 1 then
+				Console_Print(string.format("AI making a blocking move at cell index %d", i));
+				PlacePiece(PLAYER_AI, bit)
+				return 1
+			end
+		end
+	end
+
+	-- just find a blank and place a piece ffs
+	local i = 1
+	while i <= 9 do
+		local cell = math.random(1, 9)
+		local bit = 1 << (cell - 1)
+
+		if CellIsEmpty(bit) == true then
+			Console_Print(string.format("AI making random move at cell index %d", cell));
+			PlacePiece(PLAYER_AI, bit)
+			return 0
+		end
+
+		i = i + 1
+	end
+end
+
+function Update()
+	if gameState == GAME_STATE_PLAYING and turn == PLAYER_AI then
+		local result = ProcessAI()
+
+		if result == 2 then -- AI has won
+			Console_Print(string.format("Round %d LOSS!", round))
+			gameState = GAME_STATE_LOSS;
+			return
+		end
+
+		turn = PLAYER_MEAT_BAG
+	end
+
+	local gameBoard = aiBoard | playerBoard;
+
+	if gameBoard == 511 and gameState == GAME_STATE_PLAYING then
+		if CheckForPossibleWin(PLAYER_MEAT_BAG, 0) == 1 then
+			Console_Print(string.format("Round %d WIN!", round))
+			gameState = GAME_STATE_WIN
+		else
+			Console_Print(string.format("Round %d DRAW!", round))
+			gameState = GAME_STATE_DRAW
+		end
+	end
 end
 
 function Render()
